90c0cbb0f29e551f899665043ca0c9e8436b49cb
Anand Sadasivan <139990619+Anand-29@users.noreply.github.com>
2026-03-14 14:51:17 +0530

    Update Strings 1.2.py

--- Strings 1.2.py
+++ Strings 1.2.py
@@ -88,9 +88,170 @@ for i in x:
         if i not in ans:
             ans+=i
 print(f"Duplicate values in string : {ans}")
-''' 
+'''
+
+'''
+#Check two string are anagram(sort based):
+def check_anangram(s1,s2):
+    if len(s1)!=len(s2):
+        return False
+    s1=sorted(s1)
+    s2=sorted(s2)
+    if s1==s2:
+        return True
+    return False
     
-    
+s1=input("Enter the String 1 : ")
+s2=input("Enter the String 2 : ")
+ans=check_anangram(s1,s2)
+print("The string are anagram" if ans==True else "The strings are not anangram")
+'''
+
+'''
+#Check two Strings are anagram(Frequency Based):
+def check_anangram(s1,s2):
+    if len(s1) != len(s2):
+        return False
+    freq=[x*0 for x in range(26)]
+    for i in s1:
+        ind=ord(i)-97 # uppercase ascii -> 65
+        freq[ind]+=1
+    for i in s2:
+        ind=ord(i)-97
+        freq[ind]-=1
+    for i in freq:
+        if i!=0:
+            return False
+    return True
+
+s1=input("Enter the String 1 : ").lower()
+s2=input("Enter the String 2 : ").lower()
+ans=check_anangram(s1,s2)
+print("The string are anagram" if ans==True else "The strings are not anangram")
+'''
+
+'''
+#find maximum word in a String:
+s=input("Enter the String: ").split()
+max=0
+char=""
+for i in s:
+    t=str(i)
+    if len(t)>max:
+        max=len(t)
+        char=i
+print(f"The max character is '{char}' with the length of {max}")
+'''
+
+'''
+#Remove nth index
+s=input("Enter the String: ")
+ind=int(input("Enter the index : "))
+print(f"String after removing {ind} is {s[:ind]+s[ind+1:]}")
+'''
+
+'''
+#replace every space with hypen:
+s=input("Enter the string: ").replace(" ","-")
+print(s)
+'''
+
+'''
+#largest of 2 word without lne():
+s1=input("Enter the string 1: ")
+s2=input("Enter the string 2: ")
+c1,c2=0,0
+for i in s1:
+    c1+=1
+for i in s2:
+    c2+=1
+print("String 1 is largest" if c1>c2 else "String 2 is largest")
+'''
+
+'''
+#form a new string from first and last 2 characters from a string:
+s=input("Enter the String: ")
+print(f"new string : {s[:2]+s[len(s)-2:]}")
+'''
+
+'''
+#Characters in ascending order:
+x=sorted(input("Enter the string : ").lower().replace(" ",""))
+print(f"Ascending order: {''.join(x)}")
+'''
+
+'''
+#concatinate a substring in a given string:
+s=input("Enter the string : ")
+sub=input(" Enter the substring : ")
+ind=int(input("Enter the index data : "))
+print(f"The concatinated string is : {s[:ind+1] + sub + s[ind:]}")
+'''
+
+'''
+#replace str 1 with str 2:
+s1=input("Enter the string 1 : ")
+s2=input("Enter the string 2 : ")
+if s1.index(s2[0])>-1:
+    ind=s1.index(s2[0])
+    print(f"The new string is : {s1[:ind]+s2}")
+'''
+
+'''
+#highest occurance of Character:
+s=input("Enter the String: ")
+dummy=set(s)
+max=0
+char=""
+for i in dummy:
+    ans=s.count(i)
+    if ans>max:
+        max=ans
+        char=i
+print(f"{char} : {max}")
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
     
     
