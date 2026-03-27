e56bcd40b9328a6759a22980ed2054b167c70be5
Anand Sadasivan <139990619+Anand-29@users.noreply.github.com>
2026-03-27 10:45:46 +0530

    Update selection_sort.py

--- selection_sort.py
+++ selection_sort.py
@@ -3,15 +3,11 @@ n=int(input("Enter the size: "))
 li=list(map(int,input("Enter the data: ").split()))
 c=0
 for i in range(n-1):
-    flag=False
     min=i
     for j in range(i+1,n):
         c+=1
         if li[j]<li[min]:
             min=j
-            flag=True
     li[min],li[i]=li[i],li[min]
     print("After",i+1,"pass: ",li)
-    if flag is False:
-        break
 print("sorted list: ",li);print("Count: ",c)
